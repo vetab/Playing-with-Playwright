710fe1318f6315e95e208f7f9d975cf59a82592e
vetab <vetabranislav@gmail.com>
2026-01-25 20:00:01 +0100

    Delete tests/steps directory

--- tests/steps/test_e2e.py
+++
@@ -1,8 +0,0 @@
-from pytest_bdd import scenarios
-
-
-from tests.steps.login_steps import *
-from tests.steps.inventory_steps import *
-
-scenarios('../features/login.feature')
-scenarios('../features/inventory.feature')
